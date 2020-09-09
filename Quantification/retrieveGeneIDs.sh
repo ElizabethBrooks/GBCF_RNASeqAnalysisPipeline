fd792f818d3c273ba4944130a6e2e2af67f684f1
Mae <merimaat1@gmail.com>
2020-09-09 16:04:26 -0400

    Cleaned up excess files

--- Quantification/retrieveGeneIDs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Usage: retrieveGeneIDs.sh normalizedCountsFile pValue
-#Script to retrieve gene IDs with the input P-value treshold
--- util/sd.awk
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env awk
-{
-	sum+=$0;
-	a[NR]=$0
-}
-END{
-	for(i in a)y+=(a[i]-(sum/NR))^2;
-	print sqrt(y/(NR-1))
-}
