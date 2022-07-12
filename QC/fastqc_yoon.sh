6b1f475196b2ce926f9927c7e58259da15649e1f
Elizabeth Brooks <merimaat1@gmail.com>
2022-07-12 02:06:42 -0400

    Updated yoon wc comments

--- QC/fastqc_yoon.sh
+++ QC/fastqc_yoon.sh
@@ -2,7 +2,7 @@
 #$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -r n
-#$ -N qc_fastqc_yoon_jobOutput
+#$ -N fastqc_yoon_jobOutput
 #$ -pe smp 8
 #Script to perform fastqc quality control of paired end reads
 #Usage: qsub fastqc_yoon.sh
@@ -26,10 +26,14 @@ if [ $? -ne 0 ]; then
 	echo "The $qcOut directory already exsists... please remove before proceeding."
 	exit 1
 fi
+#Move to the new directory
+cd $qcOut
 #Report software version
 fastqc -version > $outputsPath"/summary.txt"
 #Loop through all forward and reverse reads and run trimmomatic on each pair
 for f1 in "$readPath"/*_R1_001.fastq.gz; do
+	#Print status message
+	echo "Processing $f1"
 	#Trim extension from current file name
 	curSample=$(echo $f1 | sed 's/._001\.fastq\.gz//')
 	#Trim file path from current file name
@@ -38,20 +42,22 @@ for f1 in "$readPath"/*_R1_001.fastq.gz; do
 	#Set paired file name
 	f2=$curSample"2_001.fastq.gz"
 	#Perform QC on both paired end reads for the current sample
-	fastqc $f1 --extract
-	fastqc $f2 --extract
+	fastqc $f1 -o $qcOut --extract
+	fastqc $f2 -o $qcOut --extract
 	#Quickly check the QC results of the first pair
-	if grep -iF "WARN" $curSample"1_fastqc/summary.txt"; then
-		grep -iF "WARN" $curSample"1_fastqc/summary.txt" > $qcOut"/"$curSampleNoPath"fastqc_report.txt"
+	if grep -iF "WARN" $curSample"1_001_fastqc/summary.txt"; then
+		grep -iF "WARN" $curSample"1_001_fastqc/summary.txt" > $curSampleNoPath"1_001_fastqc_report.txt"
 	fi
-	if grep -iF "FAIL" $curSample"1_fastqc/summary.txt"; then
-		grep -iF "FAIL" $curSample"1_fastqc/summary.txt" > $qcOut"/"$curSampleNoPath"fastqc_report.txt"
+	if grep -iF "FAIL" $curSample"1_001_fastqc/summary.txt"; then
+		grep -iF "FAIL" $curSample"1_001_fastqc/summary.txt" > $curSampleNoPath"1_001_fastqc_report.txt"
 	fi
 	#Quickly check the QC results of the second pair
-	if grep -iF "WARN" $curSample"2_fastqc/summary.txt"; then
-		grep -iF "WARN" $curSample"2_fastqc/summary.txt" > $qcOut"/"$curSampleNoPath"fastqc_report.txt"
+	if grep -iF "WARN" $curSample"2_001_fastqc/summary.txt"; then
+		grep -iF "WARN" $curSample"2_001_fastqc/summary.txt" > $curSampleNoPath"2_001_fastqc_report.txt"
 	fi
-	if grep -iF "FAIL" $curSample"2_fastqc/summary.txt"; then
-		grep -iF "FAIL" $curSample"2_fastqc/summary.txt" > $qcOut"/"$curSampleNoPath"fastqc_report.txt"
+	if grep -iF "FAIL" $curSample"2_001_fastqc/summary.txt"; then
+		grep -iF "FAIL" $curSample"2_001_fastqc/summary.txt" > $curSampleNoPath"2_001_fastqc_report.txt"
 	fi
 done
+#Print status message
+echo "Analysis complete!"
