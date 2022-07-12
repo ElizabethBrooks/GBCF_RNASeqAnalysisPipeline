4e171abd0d07cd69c5b552c718c2daef76012f66
Elizabeth Brooks <merimaat1@gmail.com>
2022-07-12 15:16:44 -0400

    Adjusted scripts for multiple projects

--- QC/fastqc_yoon.sh
+++ QC/fastqc_yoon.sh
@@ -2,24 +2,32 @@
 #$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -r n
-#$ -N fastqc_yoon_jobOutput
+#$ -N fastqc_fastqc_projects_jobOutput
 #$ -pe smp 8
 #Script to perform fastqc quality control of paired end reads
-#Usage: qsub fastqc_yoon.sh
-#Usage Ex: qsub fastqc_yoon.sh
+#Usage: qsub fastqc_projects.sh inputsFile
+#Usage Ex: qsub fastqc_projects.sh inputPaths_yoon_adipocyte_July2022.txt
+#Usage Ex: qsub fastqc_projects.sh inputPaths_yoon_junkrat_July2022.txt
 
 #Required modules for ND CRC servers
 module load bio
+
+#Retrieve input argument of a inputs file
+inputsFile=$1
+
 #Retrieve paired reads absolute path for alignment
-readPath=$(grep "pairedReads:" ../InputData/inputPaths_yoon.txt | tr -d " " | sed "s/pairedReads://g")
+readPath=$(grep "pairedReads:" ../"InputData/"$inputsFile | tr -d " " | sed "s/pairedReads://g")
 #Retrieve adapter absolute path for alignment
-adapterPath=$(grep "adapter:" ../InputData/inputPaths_yoon.txt | tr -d " " | sed "s/adapter://g")
-#Retrieve trimming outputs absolute path
-outputsPath=$(grep "qc:" ../InputData/outputPaths_yoon.txt | tr -d " " | sed "s/qc://g")
-#Move to outputs directory
-cd $(dirname $outputsPath)
-#Make a new directory for trimming
-qcOut=$outputsPath
+adapterPath=$(grep "adapter:" ../"InputData/"$inputsFile | tr -d " " | sed "s/adapter://g")
+#Retrieve qc outputs absolute path
+outputsPath=$(grep "outputs:" ../"InputData/"$inputsFile | tr -d " " | sed "s/outputs://g")
+
+#Make a new directory for project analysis
+projectDir=$(basename $readPath)
+outputsPath=$outputsPath"/"$projectDir
+
+#Make a new directory for qc
+qcOut=$outputsPath"/qc"
 mkdir $qcOut
 #Check if the folder already exists
 if [ $? -ne 0 ]; then
@@ -28,6 +36,7 @@ if [ $? -ne 0 ]; then
 fi
 #Move to the new directory
 cd $qcOut
+
 #Report software version
 fastqc -version > "summary.txt"
 #Loop through all forward and reverse reads and run trimmomatic on each pair
