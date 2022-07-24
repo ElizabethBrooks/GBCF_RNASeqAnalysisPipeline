3835d5cacb906e71b282313d4e5c8a5d744bb1b7
Elizabeth Brooks <merimaat1@gmail.com>
2022-07-24 16:48:51 -0400

    Updated DE analysis script

--- DEAnalysis/edgeR_projects.r
+++ DEAnalysis/edgeR_projects.r
@@ -14,16 +14,16 @@ library(edgeR)
 library(ggplot2)
 
 #Set working directory
-setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq")
+#setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq")
 #setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq/subset")
-#setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq")
+setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq")
 #setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq/subset")
 
 #Import gene count data
 #inputTable <- read.csv(file=args[1], header = TRUE, sep = "\t", row.names="gene")
-inputTable <- read.table(file="220705_Yoon_Adipocyte_Pool2_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
+#inputTable <- read.table(file="220705_Yoon_Adipocyte_Pool2_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
 #inputTable <- read.table(file="220705_Yoon_Adipocyte_Pool2_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")[,1:12]
-#inputTable <- read.table(file="220707_Yoon_Jurkat_Pool1_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
+inputTable <- read.table(file="220707_Yoon_Jurkat_Pool1_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
 #inputTable <- read.table(file="220707_Yoon_Jurkat_Pool1_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")[,1:12]
 
 #Trim the data table
@@ -44,12 +44,12 @@ list <- DGEList(counts=countsTable,group=group)
 #Prep Stage
 
 #Plot the library sizes before normalization
-jpeg("exactTest_plotBarsBefore.jpg")
+jpeg("plotBars_librarySizes.jpg")
 barplot(list$samples$lib.size*1e-6, names=1:numSamples, ylab="Library size (millions)")
 dev.off()
 #Draw a MDS plot to show the relative similarities of the samples
 # and to view batch and treatment effects before normalization
-jpeg("exactTest_plotMDSBefore.jpg")
+jpeg("plotMDS_beforeNormalize.jpg")
 plotMDS(list, col=rep(1:3, each=4))
 dev.off()
 #Draw a heatmap of individual RNA-seq samples using moderated 
@@ -68,23 +68,19 @@ list <- list[keep, , keep.lib.sizes=FALSE]
 list <- calcNormFactors(list)
 #Write normalized counts to file
 normList <- cpm(list, normalized.lib.sizes=TRUE)
-write.table(normList, file="exactTest_normalizedCounts.csv", sep=",", row.names=TRUE)
+write.table(normList, file="normalized_counts.csv", sep=",", row.names=TRUE)
 #View normalization factors
 list$samples
 dim(list)
 
-#Plot the library sizes after normalization
-jpeg("exactTest_plotBarsAfter.jpg")
-barplot(list$samples$lib.size*1e-6, names=1:numSamples, ylab="Library size (millions)")
-dev.off()
 #Draw a MDS plot to show the relative similarities of the samples
 # and to view batch and treatment effects after normalization
-jpeg("exactTest_plotMDSAfter.jpg")
+jpeg("plotMDS_afterNormalize.jpg")
 plotMDS(list, col=rep(1:3, each=4))
 dev.off()
 #Draw a heatmap of individual RNA-seq samples using moderated
 # log-counts-per-million after normalization
-jpeg("exactTest_plotHeatMapAfter.jpg")
+jpeg("plotHeatMap_afterNormalize.jpg")
 logcpm <- cpm(list, log=TRUE)
 heatmap(logcpm)
 dev.off()
@@ -94,7 +90,7 @@ dev.off()
 list <- estimateDisp(list)
 list$common.dispersion
 #View dispersion estimates and biological coefficient of variation
-jpeg("exactTest_plotBCV.jpg")
+jpeg("plotBCV.jpg")
 plotBCV(list)
 dev.off()
 
