308245235e597b0bba94fd8e31ec213dbe555893
Elizabeth Brooks <merimaat1@gmail.com>
2022-08-01 23:56:59 -0400

    added initial script for heat maps

---
+++ DEAnalysis/edgeR_projects_heatMaps.R
@@ -0,0 +1,171 @@
+#!/usr/bin/env Rscript
+#R script to perform statistical analysis of gene count tables using edgeR exact test
+#Usage: Rscript edgeR_projects.r mergedCountsFile
+#Usage Ex: Rscript edgeR_projects.r 220705_Yoon_Adipocyte_Pool2_RNAseq_merged_counts_formatted.txt
+#Usage Ex: Rscript edgeR_projects.r 220707_Yoon_Jurkat_Pool1_RNAseq_merged_counts_formatted.txt
+
+#Install edgeR, this should only need to be done once
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("edgeR")
+
+#Load libraries
+library(edgeR)
+library(ggplot2)
+
+#Set working directory
+setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq/gene_counts")
+#setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq/gene_counts")
+
+#Import gene count data
+#inputTable <- read.csv(file=args[1], header = TRUE, sep = "\t", row.names="gene")
+#Full set
+inputTable <- read.table(file="220705_Yoon_Adipocyte_Pool2_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
+#inputTable <- read.table(file="220707_Yoon_Jurkat_Pool1_RNAseq_merged_counts_formatted.txt", header = TRUE, sep = "\t", row.names="gene")
+
+#Set working directory
+#No undetermined
+#setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq/subset_noUndetermined")
+#setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq/subset_noUndetermined")
+#DE
+setwd("/Users/bamflappy/GBCF/yoon_July2022/220705_Yoon_Adipocyte_Pool2_RNAseq/differential_expression")
+#setwd("/Users/bamflappy/GBCF/yoon_July2022/220707_Yoon_Jurkat_Pool1_RNAseq/differential_expression")
+
+#Subset the input counts
+#Full set
+#subsetTable <- inputTable
+#No undetermined
+#subsetTable <- inputTable[,1:12]
+#DE Adipocyte
+subsetTable <- inputTable[ , -which(names(inputTable) %in% c("S00", "S01", "S06", "S10"))]
+#DE Jurkat
+#subsetTable <- inputTable[ , -which(names(inputTable) %in% c("S00", "S04", "S07", "S09"))]
+
+#Trim the data table
+countsTable <- head(subsetTable, - 5)
+
+#Set number of samples
+#Full set
+#numSamples <- 13
+#No undetermined
+#numSamples <- 12
+#DE subset
+numSamples <- 9
+
+#Add grouping factor
+#Full set
+#group <- factor(c(rep("100mV",4), rep("180mV",4), rep("CTL",4), "Undetermined"))
+#No undetermined
+#group <- factor(c(rep("100mV",4), rep("180mV",4), rep("CTL",4)))
+#DE subset
+group <- factor(c(rep("100mV",3), rep("180mV",3), rep("CTL",3)))
+
+#Create DGE list object
+list <- DGEList(counts=countsTable,group=group)
+
+
+#Prep Stage
+
+#Retrieve library sizes
+libraries <- data.frame(
+  samples = names(countsTable),
+  sizes = list$samples$lib.size*1e-6)
+#Plot the library sizes before normalization
+#barplot(list$samples$lib.size*1e-6, names=1:numSamples, ylab="Library size (millions)")
+jpeg("plotBars_librarySizes.jpg")
+ggplot(data = libraries, aes(x = samples, y = sizes)) + 
+  geom_bar(stat="identity") +
+  labs(x = "Sample", y="Library size (millions)")
+dev.off()
+
+#There is no purpose in analyzing genes that are not expressed in either 
+# experimental condition, so genes are first filtered on expression levels
+keep <- filterByExpr(list)
+list <- list[keep, , keep.lib.sizes=FALSE]
+#Calculate normalized factors
+list <- calcNormFactors(list)
+#Write normalized counts to file
+normList <- cpm(list, normalized.lib.sizes=TRUE)
+write.table(normList, file="normalized_counts.csv", sep=",", row.names=TRUE)
+
+#Draw a MDS plot to show the relative similarities of the samples
+# and to view batch and treatment effects after normalization
+jpeg("plotMDS_afterNormalize.jpg")
+#Full set
+#plotMDS(list)
+#No undetermined
+#plotMDS(list, col=rep(1:3, each=4))
+#DE
+plotMDS(list, col=rep(1:3, each=3))
+dev.off()
+#Draw a heatmap of individual RNA-seq samples using moderated
+# log-counts-per-million after normalization
+jpeg("plotHeatMap_afterNormalize.jpg")
+logcpm <- cpm(list, log=TRUE)
+heatmap(logcpm)
+dev.off()
+
+#Produce a matrix of pseudo-counts
+#Estimate common dispersion and tagwise dispersions
+list <- estimateDisp(list)
+#View dispersion estimates and biological coefficient of variation
+jpeg("plotBCV.jpg")
+plotBCV(list)
+dev.off()
+
+
+#DEA Stage
+
+#Setup color vector for plotting
+color_subset <- c("#0000FF", "#000000", "#FF0000")
+
+#Perform an exact test for 100mV vs CTL
+tested <- exactTest(list, pair=c("CTL", "100mV"))
+#Create results table of DE genes
+resultsTbl <- topTags(tested, n=nrow(tested$table))$table
+write.table(resultsTbl, file="100mV_CTL_topTags.csv", sep=",", row.names=TRUE)
+#Plot log-fold change against log-counts per million, with DE genes highlighted
+#The blue lines indicate 2-fold changes
+jpeg("100mV_CTL_plotMD.jpg")
+plotMD(tested)
+abline(h=c(-1, 1), col="grey")
+dev.off()
+#Identify significantly DE genes
+resultsTbl$sigDE <- "NA"
+resultsTbl$sigDE[resultsTbl$logFC > 1 & resultsTbl$FDR < 0.05] <- "UP"
+resultsTbl$sigDE[resultsTbl$logFC < -1 & resultsTbl$FDR < 0.05] <- "DOWN"
+#Create heatmap
+
+#Perform an exact test for 180mV vs CTL
+tested <- exactTest(list, pair=c("CTL", "180mV"))
+#Create results table of DE genes
+resultsTbl <- topTags(tested, n=nrow(tested$table))$table
+write.table(resultsTbl, file="180mV_CTL_topTags.csv", sep=",", row.names=TRUE)
+#Plot log-fold change against log-counts per million, with DE genes highlighted
+#The blue lines indicate 2-fold changes
+jpeg("180mV_CTL_plotMD.jpg")
+plotMD(tested)
+abline(h=c(-1, 1), col="grey")
+dev.off()
+#Identify significantly DE genes
+resultsTbl$sigDE <- "NA"
+resultsTbl$sigDE[resultsTbl$logFC > 1 & resultsTbl$FDR < 0.05] <- "UP"
+resultsTbl$sigDE[resultsTbl$logFC < -1 & resultsTbl$FDR < 0.05] <- "DOWN"
+#Create heatmap
+
+#Perform an exact test for 180mV vs 100mV
+tested <- exactTest(list, pair=c("100mV", "180mV"))
+#Create results table of DE genes
+resultsTbl <- topTags(tested, n=nrow(tested$table))$table
+write.table(resultsTbl, file="180mV_100mV_topTags.csv", sep=",", row.names=TRUE)
+#Plot log-fold change against log-counts per million, with DE genes highlighted
+#The blue lines indicate 2-fold changes
+jpeg("180mV_100mV_plotMD.jpg")
+plotMD(tested)
+abline(h=c(-1, 1), col="grey")
+dev.off()
+#Identify significantly DE genes
+resultsTbl$sigDE <- "NA"
+resultsTbl$sigDE[resultsTbl$logFC > 1 & resultsTbl$FDR < 0.05] <- "UP"
+resultsTbl$sigDE[resultsTbl$logFC < -1 & resultsTbl$FDR < 0.05] <- "DOWN"
+#Create heatmap
